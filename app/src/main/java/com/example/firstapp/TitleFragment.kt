87e5f8b6bd4810ad0cc0056b831b89d050371677
Thitinun <thitinun.mi@rmuti.ac.th>
2020-07-31 20:05:37 +0700

    Add class & Add binding

--- app/src/main/java/com/example/firstapp/TitleFragment.kt
+++ app/src/main/java/com/example/firstapp/TitleFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.firstapp.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +31,11 @@ class TitleFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title,container,false)
+        return binding.root
     }
 
     companion object {
