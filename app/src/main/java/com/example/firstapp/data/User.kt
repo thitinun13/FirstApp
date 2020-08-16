d9b104e580b91682f10fb6f88a302d4dc9a4518e
Thitinun <thitinun.mi@rmuti.ac.th>
2020-08-16 18:05:20 +0700

    Update Class User

--- app/src/main/java/com/example/firstapp/data/User.kt
+++ app/src/main/java/com/example/firstapp/data/User.kt
@@ -1,4 +1,13 @@
 package com.example.firstapp.data
 
-class User {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val firstName: String,
+    val Email: String,
+    val phoneNumber: Int
+)
